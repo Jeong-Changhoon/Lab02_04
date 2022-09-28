dee645f85cc90503d453a6a5fe8ba5db7d101caa
정창훈 <wjdckdgns321@naver.com>
2022-09-28 22:38:51 +0900

    test

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ void renderScene()
 	glLoadIdentity();	
 	gluPerspective(45.0f, 1.0f, 0.1f, 100.0f);
 
-	glMatrixMode(GL_MODELVIEW);
+	// glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	gluLookAt(0.0f, 10.0f, 10.0f, 0.0f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f);
 
